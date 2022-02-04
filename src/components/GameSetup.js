0ec5120bfbd2e6980b5aa7713201b2332ce2cd3a
Walter Fritsche <wfritsche95@protonmail.com>
2022-02-04 15:15:36 -0600

    added classnames

--- src/components/GameSetup.js
+++ src/components/GameSetup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import styles from './GameSetup.module.css';
 
 export const GameSetup = (props) => {
   const [level, setLevel] = useState('easy');
@@ -16,23 +17,23 @@ export const GameSetup = (props) => {
     props.scoreDispatch({type: 'reset'});
   }
   return (
-    <div className='game-setup-modal'>
+    <div className={styles.wrapper}>
       <form onSubmit={formSubmitHandler}>
         <div className='level-input-group' onChange={levelChange}>
           <p>Difficulty level?</p>
-          <div>
+          <div className={styles.inputpair}>
             <input type="radio" id='easy' name='level' value='easy'/>
             <label htmlFor='easy'>Easy</label>
           </div>
-          <div>
+          <div className={styles.inputpair}>
             <input type="radio" id='medium' name='level' value='medium'/>
             <label htmlFor='medium'>Medium</label>
           </div>
-          <div>
+          <div className={styles.inputpair}>
             <input type="radio" id='hard' name='level' value='hard'/>
             <label htmlFor='hard'>Hard</label>
           </div>
-          <div>
+          <div className={styles.inputpair}>
             <input type="radio" id='expert' name='level' value='expert'/>
             <label htmlFor='expert'>Expert</label>
           </div>
