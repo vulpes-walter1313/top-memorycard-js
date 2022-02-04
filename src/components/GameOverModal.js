691e91aa5f3a5af404bab2da2a38741f35e365d2
Walter Fritsche <wfritsche95@protonmail.com>
2022-02-04 13:54:31 -0600

    added logic to change message depending if the game was won or not

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -3,7 +3,11 @@ import React from 'react';
 const GameOverModal = (props) => {
   return (
   <div className='game-over-modal'>
-    <p>Game over! Your score was {props.lastScore} with {props.bestScore} being the best score so far</p>
+    {
+      props.gameWon ? 
+      <p>You Won! You got {props.lastScore} out of {props.lastScore}</p> :
+      <p>Game over! Your score was {props.lastScore} with {props.bestScore} being the best score so far</p>
+    }
   </div>
   );
 };
