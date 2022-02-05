c076525e62079f8badb56dbc2928da68199d5a5c
Walter Fritsche <wfritsche95@protonmail.com>
2022-02-05 16:37:48 -0600

    style and functionality fix to make model disappear on click

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import styles from './GameOverModal.module.css';
 
 const GameOverModal = (props) => {
+  const { setEndGameModal } = props;
+
   return (
-  <div className='game-over-modal'>
-    {
-      props.gameWon ? 
-      <p>You Won! You got {props.lastScore} out of {props.lastScore}</p> :
-      <p>Game over! Your score was {props.lastScore} with {props.bestScore} being the best score so far</p>
-    }
+  <div className={styles.wrapper} onClick={() => setEndGameModal(false)}>
+    <div className={styles.content}>
+      {
+        props.gameWon ? 
+        <p>You Won! You got {props.lastScore} out of {props.lastScore}</p> :
+        <p>Game over! Your score was {props.lastScore} with {props.bestScore} being the best score so far</p>
+      }
+    </div>
   </div>
   );
 };
